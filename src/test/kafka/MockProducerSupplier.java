9c91d7ce4cf92256b77e2312b508cc55254454ec
Marc Mendoza <mmarc30@gmail.com>
2020-12-09 10:08:37 -0500

    refactor tests

--- src/test/kafka/MockProducerSupplier.java
+++ src/test/kafka/MockProducerSupplier.java
@@ -6,14 +6,23 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.function.Supplier;
 
 public class MockProducerSupplier<K, V> implements Supplier<Producer<K, V>> {
-    private static final Logger logger = LoggerFactory.getLogger("console");
+    private static final Logger logger = LoggerFactory.getLogger(MockProducerSupplier.class);
+    Queue<MockProducer> globalQueue = new ConcurrentLinkedDeque<>();
 
     @Override
     public Producer<K, V> get() {
-        logger.info("Creating new MockProducer...");
-        return new MockProducer(false, new StringSerializer(), new StringSerializer());
+        logger.info("Thread {} is creating new MockProducer...", Thread.currentThread().getName());
+        final MockProducer mockProducer = new MockProducer(false, new StringSerializer(), new StringSerializer());
+        globalQueue.add(mockProducer);
+        return mockProducer;
+    }
+
+    public long getCountSentMessages() {
+        return globalQueue.stream().mapToInt(p -> p.history().size()).sum();
     }
 }
--- src/test/kafka/TransactionalProducerTest.java
+++ src/test/kafka/TransactionalProducerTest.java
@@ -5,7 +5,6 @@ import com.salesforce.kafka.test.junit4.SharedKafkaTestResource;
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -14,7 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Clock;
-import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
@@ -25,8 +23,8 @@ import java.util.concurrent.Executors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class KafkaTest {
-    private final static Logger logger = LoggerFactory.getLogger(KafkaTest.class);
+public class TransactionalProducerTest {
+    private final static Logger logger = LoggerFactory.getLogger(TransactionalProducerTest.class);
     private final static int MESSAGE_COUNT = 1000;
     private final static int THREAD_COUNT = 5;
     private final static int TASK_COUNT = 50;
@@ -49,31 +47,20 @@ public class KafkaTest {
     @Test
     public void testTransactionalProducer() {
         final KafkaTestUtils kafkaTestUtils = sharedKafkaTestResource.getKafkaTestUtils();
-
         final TransactionalProducer transactionalProducer = new TransactionalProducer(new KafkaProducerSupplier<>(createOverridingProducerTransactionalProperties()));
-
         sendMessages(transactionalProducer);
-
         final List<ConsumerRecord<byte[], byte[]>> consumerRecords = kafkaTestUtils.consumeAllRecordsFromTopic(topicName);
-
         logger.info("consumer record count: {}", consumerRecords.size());
-
         assertThat(consumerRecords.size() == TASK_COUNT * MESSAGE_COUNT);
     }
 
     @Test
     public void testTransactionalProducerWithMockSupplier() {
-        final KafkaTestUtils kafkaTestUtils = sharedKafkaTestResource.getKafkaTestUtils();
-
-        final TransactionalProducer transactionalProducer = new TransactionalProducer(new MockProducerSupplier<>());
-
+        final MockProducerSupplier supplier = new MockProducerSupplier<>();
+        final TransactionalProducer transactionalProducer = new TransactionalProducer(supplier);
         sendMessages(transactionalProducer);
-
-        final List<ConsumerRecord<byte[], byte[]>> consumerRecords = kafkaTestUtils.consumeAllRecordsFromTopic(topicName);
-
-        logger.info("consumer record count: {}", consumerRecords.size());
-
-        assertThat(consumerRecords.size() == 0);
+        logger.info("consumer record count: {}", supplier.getCountSentMessages());
+        assertThat(supplier.getCountSentMessages() == TASK_COUNT * MESSAGE_COUNT);
     }
 
     private void sendMessages(TransactionalProducer transactionalProducer) {
@@ -104,26 +91,11 @@ public class KafkaTest {
         });
     }
 
-    private ConsumerRecords consumeRecords(Consumer consumer) {
-        ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofSeconds(5));
-        logger.info("[METRICS] record count: {}", consumerRecords.count());
-        for (ConsumerRecord consumerRecord : consumerRecords) {
-            logger.info("offset: {}, message: {}", consumerRecord.offset(), consumerRecord.value());
-        }
-        return consumerRecords;
-    }
-
     private static void simulateWork(TransactionalProducer transactionalProducer, String topicName) throws InterruptedException {
         Thread.sleep(1000L);
         for (int i = 0; i < MESSAGE_COUNT; i++) {
             transactionalProducer.send(new ProducerRecord<>(topicName, Thread.currentThread().getName() + " message " + i));
         }
-    }
-
-    private Consumer createConsumer(Properties properties) {
-        final Consumer consumer = new KafkaConsumer(properties);
-        consumer.subscribe(Collections.singleton(topicName));
-        return consumer;
     }
 
     private Properties createOverridingProducerTransactionalProperties() {
@@ -136,17 +108,6 @@ public class KafkaTest {
         producerProperties.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "prod");
         producerProperties.put(ProducerConfig.ACKS_CONFIG, "all");
         return producerProperties;
-    }
-
-    private Properties createOverridingConsumerTransactionalProperties() {
-        Properties consumerProperties = new Properties();
-        consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, sharedKafkaTestResource.getKafkaConnectString());
-        consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        consumerProperties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-        consumerProperties.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
-        consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group");
-        return consumerProperties;
     }
 
     private KafkaTestUtils getKafkaTestUtils() {
